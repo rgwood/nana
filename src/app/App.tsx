b2bade89792f1583d1959fe045eccb8f457e8964
Reilly Wood <reilly.wood@icloud.com>
2022-06-18 18:35:53 -0700

    fix bg color

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,7 +4,7 @@ import { invoke } from "@tauri-apps/api/tauri";
 export default function () {
     return (
         <main>
-            <h1 className="bg-blue-300 text-blue-700">Hello, world!!</h1>
+            <h1 className="bg-blue-100 text-blue-700 text-2xl text-center">Hello world!</h1>
         </main>
     );
 };
